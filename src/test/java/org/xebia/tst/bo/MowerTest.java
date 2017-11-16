777443358306b34f9e27d618fa54a9129997bbb4
BenHaddou El-Mehdi (EXTERNAL) <EM.BenHaddou@hq.eapl.europcar.vwfs-ad>
2017-11-16 16:49:13 +0100

    add TU for Mower BO

--- src/test/java/org/xebia/tst/bo/MowerTest.java
+++ src/test/java/org/xebia/tst/bo/MowerTest.java
@@ -1,4 +1,9 @@
 package org.xebia.tst.bo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
  * 
  * Test de comportement des tondeuses
@@ -9,6 +14,38 @@ package org.xebia.tst.bo;
 public class MowerTest {
 
 	private Mower mower;
-
 	
+	private Coordonnees  coordonneesBorder=new Coordonnees(5, 5);
+
+	@Test
+	public void test_creation(){
+		Coordonnees coordonnees =new Coordonnees(0, 5);
+		mower = new Mower(coordonnees, Orientation.E, false);
+		assertEquals(mower.getCoordonnees().getY(), 5);
+		assertEquals(mower.getCoordonnees().getX(), 0);
+		assertEquals(mower.getOrientation(), Orientation.E);
+		assertEquals(mower.isStoped(), false);
+	}
+	
+	@Test
+	public void test_moveMower(){
+		Coordonnees coordonnees =new Coordonnees(4, 5);
+		mower = new Mower(coordonnees, Orientation.E, false);
+		mower.moveMower(coordonneesBorder);
+		assertEquals(mower.getCoordonnees().getY(), 5);
+		assertEquals(mower.getCoordonnees().getX(), 5);
+		assertEquals(mower.getOrientation(), Orientation.E);
+		assertEquals(mower.isStoped(), false);
+	}
+	@Test
+	public void test_moveMower_outside(){
+		Coordonnees coordonnees =new Coordonnees(6, 5);
+		mower = new Mower(coordonnees, Orientation.E, false);
+		mower.moveMower(coordonneesBorder);
+		assertEquals(mower.getCoordonnees().getY(), 5);
+		assertEquals(mower.getCoordonnees().getX(), 6);
+		assertEquals(mower.getOrientation(), Orientation.E);
+		assertEquals(mower.isStoped(), true);
+		
+	}
 }
